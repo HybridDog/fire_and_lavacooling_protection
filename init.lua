82d3fcf7ecbca0366c8ef1f1bd562dd0734b042b
HybridDog <adresse_test@t-online.de>
2015-01-24 10:30:26 +0100

    add code

--- init.lua
+++ init.lua
@@ -1,5 +1,16 @@
 local load_time_start = os.clock()
 
+local function cooling_allowed(pos)
+	return not minetest.is_protected(pos, " ")
+end
 
+for _,i in pairs({"flowing", "source"}) do
+	local cool = default["cool_lava_"..i]
+	default["cool_lava_"..i] = function(pos)
+		if cooling_allowed(pos) then
+			cool(pos)
+		end
+	end
+end
 
-minetest.log("info", string.format("[] loaded after ca. %.2fs", os.clock() - load_time_start))
+minetest.log("info", string.format("[lavacooling_protection] loaded after ca. %.2fs", os.clock() - load_time_start))
